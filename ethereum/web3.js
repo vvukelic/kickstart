721a4f494970f39ef108ae2227c507097cd5b72a
Vedran Vukelic <vedran.vukelic@gmail.com>
2022-03-23 10:31:50 +0100

    Page layout.

--- ethereum/web3.js
+++ ethereum/web3.js
@@ -1,7 +1,17 @@
 import Web3 from 'web3';
-
-window.ethereum.request({ method: 'eth_requestAccounts' });
-
-const web3 = new Web3(window.ethereum);
-
+ 
+let web3;
+ 
+if (typeof window !== 'undefined' && typeof window.ethereum !== 'undefined') {
+    // We are in the browser and metamask is running.
+    // window.ethereum.request({ method: 'eth_requestAccounts' });
+    web3 = new Web3(window.ethereum);
+} else {
+    // We are on the server *OR* the user is not running metamask
+    const provider = new Web3.providers.HttpProvider(
+        'https://kovan.infura.io/v3/af38ea05790c4aa49921ecf37bd84b34'
+    );
+    web3 = new Web3(provider);
+}
+ 
 export default web3;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,42 @@
 import react from "react";
+import 'semantic-ui-css/semantic.min.css'
+import { Card, Button } from "semantic-ui-react";
+import { Component } from "react/cjs/react.production.min";
+import factory from '../ethereum/factory';
+import Layout from "../components/Layout";
 
-export default () => {
-    return <h1>This is the campaign list page!!!</h1>
-};
+class CampaignIndex extends Component {
+    static async getInitialProps () {
+        const campaigns = await factory.methods.getDeployedCampaigns().call();
+        return { campaigns };
+    }
+
+    renderCampaigns() {
+        const items = this.props.campaigns.map(address => {
+            return {
+                header: address,
+                description: <a>View campaign</a>,
+                fluid: true
+            };
+        });
+        return <Card.Group items={items} />;
+    }
+
+    render () {
+        return (
+            <Layout>
+                <div>
+                    <h3>Open Campaigns</h3>
+                    {this.renderCampaigns()}
+                    <Button
+                        content="Create Campaign"
+                        icon="add circle"
+                        primary
+                    />
+                </div>
+            </Layout>
+        );
+    }
+}
+
+export default CampaignIndex;
