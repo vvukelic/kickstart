90f05081d2fc5e2b1c191e0cef2387139e9b6a6b
Vedran Vukelic <vedran.vukelic@gmail.com>
2022-03-28 22:38:43 +0200

    Custom router + error msg.

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -1,17 +1,46 @@
 import React, { Component } from 'react';
-import { Button, Form, Input } from 'semantic-ui-react';
+import { Button, Form, Input, Message } from 'semantic-ui-react';
 import Layout from '../../components/Layout';
+import factory from '../../ethereum/factory';
+import web3 from '../../ethereum/web3';
+import { Router } from '../../routes';
 
 class CampaignNew extends Component {
     state = {
-        minimumContribution: ''
+        minimumContribution: '',
+        errorMessage: '',
+        loading: false
+    };
+
+    onSubmit = async (event) => {
+        event.preventDefault();
+
+        this.setState({
+            loading: true,
+            errorMessage: ''
+        });
+
+        try {
+            const accounts = await web3.eth.getAccounts();
+            await factory.methods
+                .createCampaign(this.state.minimumContribution)
+                .send({
+                    from: accounts[0]
+            });
+
+            Router.pushRoute('/');
+        } catch (err) {
+            this.setState({ errorMessage: err.message });
+        }
+
+        this.setState({ loading: false });
     };
 
     render() {
         return (
             <Layout>
                 <h3>Create a Campaign!</h3>
-                <Form>
+                <Form onSubmit={this.onSubmit} error={!!this.state.errorMessage}>
                     <Form.Field>
                         <label>Minimum Contribution</label>
                         <Input
@@ -22,7 +51,8 @@ class CampaignNew extends Component {
                         />
                     </Form.Field>
 
-                    <Button primary>Create!</Button>
+                    <Message error header="Ooops!" content={this.state.errorMessage} />
+                    <Button loading={this.state.loading} primary>Create!</Button>
                 </Form>
             </Layout>
         );
